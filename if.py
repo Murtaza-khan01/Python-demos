e48fe0f6035a26caea061b3029abf8674f79f008
murtaza-khan01 <murtazakhan560@gmail.com>
2025-12-17 18:03:50 +0500

    add more program files

---
+++ if.py
@@ -0,0 +1,14 @@
+food= input("food:")
+eat= "yes" if food== "cake" else "no"
+print(eat)
+
+food=input("food:")
+print("sweet") if food=="cake" or food=="jalebi" else print("not sweet")
+
+
+food= input("food")
+eat='yes' if food == "cake" else "no"
+
+
+food = input("food")
+print("sweet") if food=="cake" or food == "jslebi" else print("not sweet")
---
+++ program.py
@@ -0,0 +1,14 @@
+str1="cube"
+str2="(ill)"
+print(len(str1+str2))                              
+str="this is a book.\nmy school name is khyber"
+print(len(str))
+print(str)
+
+
+
+str4="my name is mueen"
+print(str4[7])
+
+str5="my home"
+print(str5[1:4])
