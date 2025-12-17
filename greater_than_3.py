edd0a08807b5eb431e992600e3d124cda2015e38
Murtaza khan <150264689+Murtaza-khan01@users.noreply.github.com>
2025-12-17 18:28:37 +0500

    Fix print statement for greatest number check

--- greater_than_3.py
+++ greater_than_3.py
@@ -5,7 +5,7 @@ c=int(input("enter another number:"))
 if(a>b and a>c):
     print("a is greatest")
 elif(b>a and b>c):
-   print("b is greater")
+   print("b is greatest")
 elif(c>a and c>b):
     print("c is greatest")
 
