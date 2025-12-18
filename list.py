2b972b44727e573bf925d91e083eb998715cd262
murtaza-khan01 <murtazakhan560@gmail.com>
2025-12-18 18:33:00 +0500

    add a new python program_file

---
+++ list.py
@@ -0,0 +1,24 @@
+marks=[90,20,91,80,95.6]
+print(marks)
+print(marks[1])
+print(len(marks))
+
+
+###storing different data types
+
+student=["mubeen",12,90.1,"aboha"]
+print(student)
+
+####strings are immutble and lists are muttble
+student01=["mustafa",90,20]
+student[0]="mujtaba"
+print(student)
+
+
+###slicing of lists
+numbers=[90,10,100,110]
+print(numbers[1:3])
+
+####negative slicing
+marks=[10,100,20,21]
+print(marks[-1:-3])
