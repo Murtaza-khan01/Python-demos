81adcb8ba8dafef5e5cb6a2f9a645473ccabff51
murtaza-khan01 <murtazakhan560@gmail.com>
2025-12-22 19:13:00 +0500

    add new file

---
+++ dic.py
@@ -0,0 +1,40 @@
+info ={
+    "name" :'mubeen',
+    "age" : 12,
+    "marks": 94.1,
+    "surname":"khan",
+}
+print(info)
+print(info["name"])
+info["name"] = "mujtaba"
+print(info)     #####simple program of dictionaries in python
+
+marks ={
+ "subjects" : ["english","urud","maths","science"],  ###using lists in dic
+ "marks" : (94,100,102,90) #### using tuple
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+info={
+    "name":"mobeen",
+    "friend": "saad",
+    "marks":[87,98],
+    "subjects": ("enhish, urdu , maths"),
+
+}
+print(info)
+info["name"]="murtaza"
