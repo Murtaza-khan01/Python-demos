6190559af74ef42687ba495d9a3426adfa443ffd
murtaza-khan01 <murtazakhan560@gmail.com>
2025-12-26 18:33:15 +0500

    add new files

---
+++ pratice.py
@@ -0,0 +1,17 @@
+list_of_facts_and_figures={
+    "table":"a small piece of furinuture",
+    "cat":"a small animal"
+}
+print(list_of_facts_and_figures)
+
+marks={}
+x=int(input("english marks:"))
+marks.update({"english":x})
+x=int(input("math:"))
+marks.update({"math":x})
+x=int(input("science"))
+marks.update({"science":x})
+print(marks)
+
+set10={1,"1.0"}
+print(set10)
---
+++ set.py
@@ -0,0 +1,15 @@
+set1={1,2,3,5}   #### this is a simple program of sets in python with different methods:
+
+set1.add(6)           ##add is use to add an elemet to the set
+set1.remove(3)         ####remove is use to remove an elment to the set 
+set1.clear()            ####to clear the set
+print((set1))
+
+collection= {"swat","peshawar","buner"}
+print(collection.pop())
+
+
+set4={1,2,3,4}
+set5={2,3,5,6}
+print(set4.union(set5))   ####3union of a set 
+print(set4.intersection(set5))    ####intersection of set
