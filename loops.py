ee025541dd2d12d4f3f7b57eac9304acfb66614f
murtaza-khan01 <murtazakhan560@gmail.com>
2025-12-27 19:07:38 +0500

    add new file

---
+++ loops.py
@@ -0,0 +1,32 @@
+i=100
+while i<=1:
+    print(i)
+    i -= 1
+
+n=int(input("enter your number:"))
+j=1
+while j<=10:   ####stopping condition
+    print(n*j)
+    j+=1      
+
+heroes= ["ironman", "thor","superman","itachi"]
+
+idex=0
+while idex < len(heroes):
+    print(heroes[idex])
+    idex+=1
+
+m=int(input("enter your number:"))
+k=1
+while k<=10:
+    print(m*k)
+    k+=1
+
+
+heroes=["itachi","gojo","jinwoo",]
+idex=1
+while idex<len(heroes):
+    print(heroes[idex])
+    idex+=1
+
+
