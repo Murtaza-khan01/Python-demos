fd7984b589fbc1777fc1aaf7f17c19b75b23139a
murtaza-khan01 <murtazakhan560@gmail.com>
2026-01-03 19:50:18 +0500

    add new programs

---
+++ functions.py
@@ -0,0 +1,50 @@
+#####Basic functions in python
+
+def cal (a,b):
+    sum = a+b
+    print(sum)
+    return sum
+
+cal(12,2)
+
+def cal (h,p):
+    sum=h*p
+    print(sum)
+    return sum
+cal (2,3)
+
+
+
+def cal(u,e):
+    sum= u*e
+    print (sum)
+    return sum
+cal(5,6)
+def cal(a,b):
+    sum = a+b
+    print(sum)
+    return sum
+cal(20,10)
+
+def cal(s,e):
+    sum = s+e
+    print(sum)
+    return sum
+cal(3,3)
+
+def avager(c,d,f):
+    avg=c+d+f/3
+    print(avg)
+
+avager(20,20,20)  ####calculate avager of 3 numbers
+
+cities= ["swat","buneri","kalam","malamjaba"]
+heroes= ["superman", "spiderman","batman"]
+
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item, end="")
+print_list(heroes)
